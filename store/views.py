bf27b9af17ff0c41b4a4932999044fbb3408c80c
Lale <100545087+LilyMurat@users.noreply.github.com>
2023-06-20 14:43:59 -0400

    productlist view

--- store/views.py
+++ store/views.py
@@ -11,19 +11,16 @@ from .serializers import ProductSerializer, CollectionSerializer
 from django.db.models.aggregates import Count
 # Create your views here.
 
-class ProductList(APIView):
-    def get(self, request):
-        queryset = Product.objects.select_related('collection').all()
-        serializer = ProductSerializer(queryset, many=True, context={'request': request})
-        return Response(serializer.data)
+class ProductList(ListCreateAPIView):
+    def get_queryset(self):
+        return Product.objects.select_related('collection').all()
     
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        #print(serializer.validated_data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
+    def get_serializer_class(self):
+        return ProductSerializer
+    
+    def get_serializer_context(self):
+        return {'request': self.request}
+    
 class ProductDetail(APIView):
         
     def get(self, request, id):
