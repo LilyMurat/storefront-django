d9be62ea0b34cd8b6e93e4c06c8a48601a668e5d
Lale <100545087+LilyMurat@users.noreply.github.com>
2023-06-23 22:20:26 -0400

    extending the user model - core app

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.response import Response
@@ -34,10 +35,12 @@ class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count('products')).all()
     serializer_class =CollectionSerializer
 
-    def destroy(self, request, *args, **kwargs):
-        if Collection.objects.filter(product_id=kwargs['pk']).count() > 0:
+    def delete(self, request, pk):
+        collection = get_object_or_404(Collection, pk=pk)
+        if collection.products.count()>0:
             return Response({'error':'Collection cannot be deleted because it contains a product'})
-        return super().destroy(request, *args, **kwargs)
+        collection.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class ReviewViewSet(ModelViewSet):
     serializer_class = ReviewSerializer
--- storefront/settings.py
+++ storefront/settings.py
@@ -42,9 +42,9 @@ INSTALLED_APPS = [
     'playground',
     'debug_toolbar',
     'store',
-    'store_custom',
     'tags',
-    'likes'
+    'likes',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -144,8 +144,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK ={
     'COERCE_DECIMAL_TO_STRING': False, 
-    #'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.LimitOffsetPagination',
-    #'PAGE_SIZE':10
 }
 
-#.PageNumberPagination
+AUTH_USER_MODEL = 'core.User'
+
