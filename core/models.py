0cd8bc7216945929c40de365dcb956dd871b022e
Lale <100545087+LilyMurat@users.noreply.github.com>
2023-06-28 21:25:30 -0400

    get the current user - with JWT token

--- core/models.py
+++ core/models.py
@@ -2,8 +2,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+
+
     
--- store/serializers.py
+++ store/serializers.py
@@ -98,7 +98,7 @@ class AddCartItemSerializer(serializers.ModelSerializer):
 
 class CustomerSerializer(serializers.ModelSerializer):
     user_id = serializers.IntegerField()
-    
+
     class Meta: 
         model = Customer
         fields = ['id', 'user_id', 'phone', 'birth_date', 'membership']
